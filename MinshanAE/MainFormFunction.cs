718d98b555918e8b03c058c77d83c50c5cab3313
Du Chunpeng <153176541@qq.com>
2014-04-14 18:44:38 +0800

    添加label函数

--- MinshanAE/MainFormFunction.cs
+++ MinshanAE/MainFormFunction.cs
@@ -1,13 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
-using ESRI.ArcGIS.Controls;
-using ESRI.ArcGIS.Geodatabase;
-using ESRI.ArcGIS.DataSourcesGDB;
-using ESRI.ArcGIS.Carto;
 using System.Windows.Forms;
+using System.Collections.Generic;
+
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
 using ESRI.ArcGIS.Display;
+
+using ESRI.ArcGIS.DisplayUI;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.DataSourcesFile;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.DataSourcesGDB;
+using stdole;
 
 namespace MinshanAE
 {
@@ -30,6 +41,12 @@ namespace MinshanAE
             IWorkspace workspace = worFact.OpenFromFile(filePath, 0);
             IFeatureWorkspace featureWorkspace = (IFeatureWorkspace)workspace.WorkspaceFactory.OpenFromFile(filePath, m_mapControl.hWnd);
 
+            //添加行政区
+            IFeatureLayer featureLayer3 = new FeatureLayer();
+            featureLayer3.Name = "行政区";
+            featureLayer3.Visible = true;
+            featureLayer3.FeatureClass = featureWorkspace.OpenFeatureClass("行政区");
+            m_mapControl.Map.AddLayer((ILayer)featureLayer3);
             //添加植被覆盖a
             IFeatureLayer featureLayer2 = new FeatureLayer();
             featureLayer2.Name = "植被覆盖a";
@@ -58,6 +75,7 @@ namespace MinshanAE
             m_mapControl.Map.AddLayer((ILayer)featureLayer1);
 
             //生成专题图
+            //渲染植被覆盖a
             ITable pTable;
             int fieldNumber;
             string strFieldName = "NAME1";
@@ -66,7 +84,7 @@ namespace MinshanAE
             fieldNumber = pTable.FindField(strFieldName);
             if (fieldNumber == -1)
             {
-                MessageBox.Show("Can't find field called STATE_NAME");
+                MessageBox.Show("Can't find field called 植被覆盖a");
                 return;
             }
 
@@ -126,6 +144,60 @@ namespace MinshanAE
             m_mapControl.ActiveView.Refresh();
             m_tocControl.SetBuddyControl(m_mapControl.Object);
             m_tocControl.Refresh();
+        }
+
+       //使用TextElment绘制标注, fieldName为要绘制的属性
+        public static void AddLable(AxMapControl axMapControl, ILayer layer, string fieldName)
+        {
+            IRgbColor pColor = new RgbColorClass()
+            {
+                Red = 255,
+                Blue = 0,
+                Green = 0
+            };
+            IFontDisp pFont = new StdFont()
+            {
+                Name = "宋体",
+                Size = 5
+            } as IFontDisp;
+                        ITextSymbol pTextSymbol = new TextSymbolClass()
+            {
+                Color = pColor,
+                Font = pFont,
+                Size = 11
+            };
+
+            IGraphicsContainer pGraContainer = axMapControl.Map as IGraphicsContainer;
+
+            //遍历要标注的要素
+            IFeatureLayer pFeaLayer = layer as IFeatureLayer;
+            IFeatureClass pFeaClass = pFeaLayer.FeatureClass;
+            IFeatureCursor pFeatCur = pFeaClass.Search(null, false);
+            IFeature pFeature = pFeatCur.NextFeature();
+            int index = pFeature.Fields.FindField(fieldName);//要标注的字段的索引
+            IEnvelope pEnv = null;
+            ITextElement pTextElment = null;
+            IElement pEle = null;
+            while (pFeature != null)
+            {
+                //使用地理对象的中心作为标注的位置
+                pEnv = pFeature.Extent;
+                IPoint pPoint = new PointClass();
+                pPoint.PutCoords(pEnv.XMin + pEnv.Width * 0.5, pEnv.YMin + pEnv.Height * 0.5);
+
+                pTextElment = new TextElementClass()
+                {
+                    Symbol = pTextSymbol,
+                    ScaleText = true,
+                    Text = pFeature.get_Value(index).ToString()
+                };
+                pEle = pTextElment as IElement;
+                pEle.Geometry = pPoint;
+                //添加标注
+                pGraContainer.AddElement(pEle, 0);
+                pFeature = pFeatCur.NextFeature();
+            }
+            (axMapControl.Map as IActiveView).PartialRefresh(esriViewDrawPhase.esriViewGraphics, null, axMapControl.Extent);
         }
 
     }
